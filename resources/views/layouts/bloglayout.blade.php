b36fd9c97959222ae150df7fb06b300a01e8293e
cvetomirg <alanforts@gmail.com>
2018-02-08 15:53:35 +0200

    test

--- resources/views/layouts/bloglayout.blade.php
+++ resources/views/layouts/bloglayout.blade.php
@@ -6,11 +6,11 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <!-- Tell the browser to be responsive to screen width -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
+    <meta name="description" content="RaveApps - Software Products | Services ">
+    <meta name="author" content="raveapps.com">
     <!-- Favicon icon -->
     <link rel="icon" type="image/png" sizes="16x16" href="assets/images/favicon.png">
-    <title>Material Pro Admin Template - The Most Complete & Trusted Bootstrap 4 Admin Template</title>
+    <title>RaveApps - Web Development</title>
     <!-- Bootstrap Core CSS -->
     <link href="assets/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet">
     <!-- Custom CSS -->
